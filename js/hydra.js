3b870b567bcc02560cd66b15fb000023920d77be
Markus Lanthaler <mark_lanthaler@gmx.net>
2013-06-10 14:33:27 +0200

    Allow initial URL to be passed via a query parameter

--- js/hydra.js
+++ js/hydra.js
@@ -687,5 +687,18 @@ $(document).ready(function() {
   // window.HydraClient.showDocumentation();
   // //window.HydraClient.get('http://hydra.test/app_dev.php/');
   // window.HydraClient.get('http://www.markus-lanthaler.com/hydra/api-demo/');
+
+  var queryParams = {};
+  if (window.location.search.length > 1) {
+    for (var aItKey, nKeyId = 0, aCouples = window.location.search.substr(1).split("&"); nKeyId < aCouples.length; nKeyId++) {
+      aItKey = aCouples[nKeyId].split("=");
+      queryParams[unescape(aItKey[0])] = aItKey.length > 1 ? unescape(aItKey[1]) : "";
+    }
+  }
+
+  if ('url' in queryParams) {
+    $('#url').val(queryParams.url);
+  }
+
   $('#url').focus();
 });
